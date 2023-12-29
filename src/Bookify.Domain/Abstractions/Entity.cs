b4c663a8744cc6fd06e6ab898226dcdf02434054
Juan Orjuela <Juan.Orjuela@d2l.com>
2023-12-28 19:36:23 -0500

    Add project files.

---
+++ src/Bookify.Domain/Abstractions/Entity.cs
@@ -0,0 +1,13 @@
+﻿namespace Bookify.Domain.Abstractions;
+
+public abstract class Entity
+{
+    private readonly List<IDomainEvent> domainEvents = new();
+
+    protected Entity(Guid id)
+    {
+        this.Id = id;
+    }
+
+    public Guid Id { get; init; }
+}
---
+++ src/Bookify.Domain/Apartments/ValueObjects/Currency.cs
@@ -0,0 +1,22 @@
+﻿namespace Bookify.Domain.Apartments.ValueObjects;
+
+public record Currency
+{
+    internal static readonly Currency None = new("");
+
+    private static readonly Currency Usd = new("USD");
+
+    private static readonly Currency Eur = new("EUR");
+
+    private Currency(string code) => Code = code;
+
+    public string Code { get; init; }
+
+    public static Currency FromCode(string code)
+    {
+        return All.FirstOrDefault(c => c.Code.Equals(code)) ?? 
+               throw new ApplicationException("The currency code is invalid.");
+    }
+
+    private static readonly IReadOnlyCollection<Currency> All = new[] { Usd, Eur };
+}
